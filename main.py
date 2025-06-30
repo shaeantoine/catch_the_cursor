65d0fa08ec2a29a336c7324bbcb08c3f667b9076
shaeantoine <shaeantoine@gmail.com>
2025-06-30 12:36:48 +1000

    Implement OOP for particle initialization

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ pg.display.set_caption("Catch The Cursor")
 background_colour = (255, 255, 255)
 screen.fill(background_colour)
 
+# Draw a circle in the middle of the screen
+class Particle: 
+    def __init__(self, x, y, size): 
+        self.x = x
+        self.y = y
+        self.size = size 
+        self.colour = (0,0,255)
+        self.thickness = 2
+
+    def display(self): 
+        pg.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
+
+particle = Particle(150,50,15)
+particle.display()
+
 pg.display.flip()
 
 running = True
