6d7e1d754882b984b48377024d37f7366c13f4d3
shaeantoine <shaeantoine@gmail.com>
2025-06-30 12:25:56 +1000

    Create full window capabilities

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pygame as pg
 
 (width, height) = (1000, 700)
 screen = pg.display.set_mode((width, height))
+
 pg.display.set_caption("Catch The Cursor")
+background_colour = (255, 255, 255)
+screen.fill(background_colour)
+
 pg.display.flip()
 
 running = True
